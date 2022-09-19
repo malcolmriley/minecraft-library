46650aa414b2621535b62ab24bd0b1425dd8a712
Malcolm Riley <malcolmriley@users.noreply.github.com>
2022-09-18 18:25:44 -0700

    Implement dummy mod class for library

---
+++ src/main/java/paragon/minecraft/library/ParagonStash.java
@@ -0,0 +1,15 @@
+package paragon.minecraft.library;
+
+import net.minecraftforge.fml.common.Mod;
+
+/**
+ * Dummy mod for library.
+ * 
+ * @author Malcolm Riley
+ */
+@Mod(ParagonStash.MOD_ID)
+public class ParagonStash {
+	
+	public static final String MOD_ID = "paragon_library";
+
+}
